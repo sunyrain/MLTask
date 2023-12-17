8cd4c1962f401d31cfbefcbab809cdfe7e28553c
Kevin Hu <kevinhu94@gmail.com>
2023-12-17 10:25:20 +0800

    pre labelling

---
+++ preprocess_dataset.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import os
+
+
+status = ['AddWeight', 'Normal', 'PressureGain_constant', 'PropellerDamage_bad', 'PropellerDamage_slight']
+def intercept_datapoints(AW_Data):
+    if AW_Data[:,1].shape[0] >= 181:    # Determine if the collected data points exceed 180
+        data_E1 = AW_Data[1:181,1:]     # Intercept the first 180 values and discard the header
+        data_E1 = normal(data_E1)   # Standardize the signal
+        return data_E1
+    else:
+        AW_Data = normal(AW_Data[1:, 1:])  # Standardize the signal
+        return AW_Data
+
+
+def normal(data):
+    max_values = [1200, 1200, 1200, 1200, 0.5, 700, 12, 15, 10, 200, 20, 10, 10, 10, 30, 40]
+    data = data.astype(np.float32)
+    for id in range(16):
+        data[:, id] = data[:, id] / max_values[id]
+    return data
+
+
+# Iterate through the directory structure
+for root, dirs, files in os.walk('Dataset'):
+
+    # Check if we are in a directory containing CSV files
+    if any(file.lower().endswith('.csv') for file in files):
+        # Process each CSV file in the current directory
+        for file in files:
+            print(file)
+            if file.lower().endswith('.csv'):
+                file_path = os.path.join(root, file)
+                if not os.path.exists(os.path.join('dataset_preprocessed', root)):
+                    os.makedirs(os.path.join('dataset_preprocessed', root))
+
+                AW_Data = pd.read_csv(str(file_path), header=None)
+                AW_Data = np.array(AW_Data)  # Convert to numpy format
+                output = intercept_datapoints(AW_Data)
+                output = normal(output)
+
+                np.save(os.path.join('dataset_preprocessed', file_path), output)
+
