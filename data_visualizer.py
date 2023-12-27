ce4b273355c8dccb99af986b4adea68317f713f2
Kevin Hu <kevinhu94@gmail.com>
2023-12-27 10:14:44 +0800

    fix preprocess_dataset_rfc.py bug of additional empty matrix

--- data_visualizer.py
+++ data_visualizer.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import numpy as np
-
+import seaborn as sns
 
 possible_status = ['AddWeight',
                    'Normal',
@@ -10,36 +10,58 @@ possible_status = ['AddWeight',
                    'PropellerDamage_bad',
                    'PropellerDamage_slight']
 
-def data_visualization(input_data, file_name):
-    t = input_data[:, 0]
-    for i in range(input_data.shape[1]-1):
-        y = input_data[:, i+1]
-        #plt.plot(t[1:], y[1:])
-        plt.plot(y[1:])
-        plt.xlabel(t[0])
-        plt.ylabel(y[0])
-        plt.title(file_name)
-        plt.grid()
-        plt.show()
-        user_input = input('Enter x to cancel:')
-        if user_input == 'x':
-            break
+
+def intercept_datapoints(input_data):
+    if input_data[:, 1].shape[0] >= 180:    # Determine if the collected data points exceed 180
+        output_data = input_data[0:180, :]     # Intercept the first 180 values and discard the header
+        output_data = normal(output_data)   # Standardize the signal
+        return output_data
+    else:
+        return None
 
 
-for root, dirs, files in os.walk('Dataset'):
-    # Check if we are in a directory containing CSV files
-    if any(file.lower().endswith('.csv') for file in files):
-        # Process each CSV file in the current directory
-        for file in files:
-            if file.lower().endswith('.csv'):
-                file_path = os.path.join(root, file)
-                label_name = root.split('/')[2]
-                if label_name in possible_status:
-                    label = possible_status.index(label_name)
-                else:
-                    label = 100
-                    print('Label not found')
+def normal(input_data: np.array):
+    max_values = [1, 1200, 1200, 1200, 1200, 0.5, 700, 12, 15, 10, 200, 20, 10, 10, 10, 30, 40]
+    input_data = input_data.astype(np.float32)
+    for id in range(len(max_values) - 1):
+        input_data[:, id] = input_data[:, id] / max_values[id]
+    return input_data
 
-                AW_Data = pd.read_csv(str(file_path), header=None)
-                AW_Data = np.array(AW_Data)  # Convert to numpy format
-                data_visualization(AW_Data, file)
+
+def data_visualization(input_data):
+    #t = input_data[:, 0]
+    #data_numerical = input_data.drop(0).astype(float)
+    #input_data.iloc[1:] = input_data.iloc[1:].astype(float)
+    pairplot = sns.pairplot(input_data)
+    fig = pairplot.fig
+    fig.savefig("pairplot.png")
+    # for i in range(input_data.shape[1]-1):
+    #     y = input_data[:, i+1]
+    #     #plt.plot(t[1:], y[1:])
+    #     plt.plot(y[1:])
+    #     plt.xlabel(t[0])
+    #     plt.ylabel(y[0])
+    #     plt.title(file_name)
+    #     plt.grid()
+    #     plt.show()
+    #     user_input = input('Enter x to cancel:')
+    #     if user_input == 'x':
+    #         break
+
+
+file_path = 'Dataset/train/AddWeight/AddWeight_0.csv'
+
+data_csv = pd.read_csv(str(file_path))
+data = np.array(data_csv)  # Convert to numpy format
+
+output = intercept_datapoints(data)
+if output is not None:
+    output = normal(output)
+    t = np.arange(1, 181)
+    output[:, 0] = t
+    df = pd.DataFrame(output, columns=['time', 'pwm1', 'pwm2', 'pwm3', 'pwm4',
+                                       'depth', 'press', 'voltage', 'roll', 'pitch', 'yaw', 'a_x', 'a_y',
+                                       'a_z', 'w_row', 'w_pitch', 'w_yaw'])
+    data_visualization(df)
+else:
+    print("Dataset " + file_path + " < 180 datapoints --> won't be included")
--- randomforestclassifier.py
+++ randomforestclassifier.py
@@ -6,28 +6,38 @@ from sklearn.ensemble import RandomForestClassifier
 from torch.utils.data import TensorDataset, DataLoader
 import torch
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 import seaborn as sns
+from sklearn.decomposition import PCA
 
-train_set = np.load('trainset.npy')
-test_set = np.load('testset.npy')
+
+train_set = np.load('train.npy')
+test_set = np.load('test.npy')
+
 
 possible_status = ['0: AddWeight', '1: Normal', '2: PressureGain_constant', '3: PropellerDamage_bad', '4: PropellerDamage_slight']
 
-X_train = train_set[:, 0:16]
-y_train = train_set[:, 16]
-X_test = test_set[:, 0:16]
-y_test = test_set[:, 16]
+X_train = train_set[:, 0:-1]
+y_train = train_set[:, -1]
+X_test = test_set[:, 0:-1]
+y_test = test_set[:, -1]
 
-rfc = RandomForestClassifier(random_state=3407, n_estimators=1000)
+
+rfc = RandomForestClassifier(random_state=3407)
+
+#X_train, X_test = apply_feature_extraction(X_train, X_test, y_train)
+
 
 rfc = rfc.fit(X_train, y_train)
 
-score_r = rfc.score(X_test, y_test)
-
-print("Random Forest: {}" .format(score_r))
-
 y_pred = rfc.predict(X_test)
+
+score_r = rfc.score(X_test, y_test)
+print("Random Forest: {}" .format(score_r))
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+print(classification_report(y_test, y_pred))
+
 
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
